74922ba0c273e73fcacce94b912befc55c2689c9
nawcodes <nawcodes@gmail.com>
2023-07-20 21:53:11 +0700

    feat: new input todo

--- resources/js/Pages/Todo/Index.jsx
+++ resources/js/Pages/Todo/Index.jsx
@@ -6,12 +6,21 @@ import ListTodo from "../../Component/ListTodo";
 
 const Index = () => {
     return (
-        <div className="grid grid-row-1 h-screen">
-            <div className="grid grid-cols-3 gap-1">
-                <InputSection></InputSection>
-                <ListTodo></ListTodo>
+        <>
+            <div className="flex flex-col justify-center items-center h-screen">
+                <div className="bg-white w-1/3 rounded p-2 shadow-lg cursor-pointer">
+                    <input
+                        type="text"
+                        placeholder="Type Here"
+                        className="w-1/2 border-none bg-inherit focus:ring-0 focus:outline-none"
+                        
+                    />
+                </div>
+                <div className="flex w-1/3 py-2 text-slate-500">
+                    <p className="mx-1 hover:underline cursor-pointer">All (28)</p>
+                </div>
             </div>
-        </div>
+        </>
     );
 };
 
