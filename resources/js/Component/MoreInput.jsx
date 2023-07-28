f7e637d9061bf3944a55429b057fe3c8e3765588
nawcodes <nawcodes@gmail.com>
2023-07-28 13:31:18 +0700

    handle values

--- resources/js/Component/MoreInput.jsx
+++ resources/js/Component/MoreInput.jsx
@@ -1,13 +1,17 @@
-const MoreInput = () => {
+const MoreInput = ({onKeyup}) => {
+
+
     return (
         <>
             {/* Detail Input */}
-            <div className="w-full">
+            <div className="w-full ">
                 <div className="bg-white   self-beetwe mt-2 rounded p-2 shadow-lg cursor-pointer">
                     <textarea
                         type="text"
                         placeholder="Type Here"
                         className="w-full border-none bg-inherit focus:ring-0 focus:outline-none"
+                        id="description"
+                        onChange={(e) => onKeyup(e)}
                     ></textarea>
                 </div>
                 <div className="flex  mt-2">
@@ -20,6 +24,9 @@ const MoreInput = () => {
                                 type="date"
                                 placeholder="Type Here"
                                 className="w-full border-none bg-inherit focus:ring-0 focus:outline-none  text-slate-500"
+                                onChange={(e) => onKeyup(e)}
+                                id="duedate"
+
                             />
                         </div>
                     </div>
@@ -30,12 +37,14 @@ const MoreInput = () => {
                         </label>
                         <div className="bg-white w-full rounded p-4 shadow-lg cursor-pointer">
                             <select
-                                id="countries"
+                            
+                                id="priority"
                                 className="w-full border-none bg-inherit focus:ring-0 focus:outline-none text-slate-500"
+                                onChange={(e) => onKeyup(e)}
                             >
-                                <option selected>Choose a Priority </option>
+                                <option defaultValue="" selected>Choose a Priority</option>
                                 <option value="none">None</option>
-                                <option value="low">Low</option>
+                                <option value="low">low</option>
                                 <option value="high">High</option>
                             </select>
                         </div>
--- resources/js/Pages/Todo/Index.jsx
+++ resources/js/Pages/Todo/Index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "@inertiajs/inertia-react";
 import Layout from "../../Layouts/Default";
 import InputSection from "../../Component/InputSection";
@@ -6,9 +6,39 @@ import ListTodo from "../../Component/ListTodo";
 import MoreInput from "../../Component/MoreInput";
 
 const Index = () => {
+    
+    const [payload, setPayload] = useState({
+        title: '',
+        description: '',
+        duedate: '',
+        priority: ''
+    });
+
+    const [isMoreInput, setIsMoreInput] = useState(false);
 
     const handleTitleClick = (e) => {
+        openMoreInput();
+    };
+
+    const openMoreInput = () => {
+        if (isMoreInput == true) {
+            setIsMoreInput(false);
+        } else {
+            setIsMoreInput(true);
+        }
+    };
+
+    function handleValues(e) {
+        const key = e.target.id;
+
+        setPayload((value) => ({
+            ...value,
+            [key]: e.target.value,
+        }));
+
     }
+
+    console.log(payload);
 
     return (
         <>
@@ -16,19 +46,29 @@ const Index = () => {
                 <input
                     type="text"
                     placeholder="Search Your do"
-                    className="w-full border-none bg-inherit focus:ring-0 focus:outline-none"
+                    className="w-full border-none bg-inherit focus:ring-0 
+                    focus:outline-none"
+                    defaultValue=""
+                    id="title"
                 />
             </div>
-            <div className="w-full flex h-screen justify-center items-center">
-                <div className="flex flex-col justify-center items-center border-2 rounded-lg shadow border-slate-400 lg:w-1/3 w-5/6 lg:h-1/2 h-5/6 p-6" >
-                    <div className="bg-white w-full rounded p-2 shadow-lg cursor-pointer" onClick={(e) => handleTitleClick(e)}>
+            <div className="w-full flex h-screen justify-center items-center ">
+                <div className="flex flex-col justify-center items-center border-2 rounded-lg shadow border-slate-400 lg:w-1/3 w-5/6 lg:h-1/2 h-5/6 p-6 overflow-auto  max-h-96">
+                    <div
+                        className="bg-white lg:2/3 w-full rounded p-2 shadow-lg cursor-pointer"
+                        onClick={(e) => handleTitleClick(e)}
+                    >
                         <input
                             type="text"
                             placeholder="Type Here"
                             className="lg:w-2/3 w-full border-none bg-inherit focus:ring-0 focus:outline-none"
+                            defaultValue=""
+                            onClick={(e) => e.stopPropagation()}
+                            onChange={handleValues}
+
                         />
                     </div>
-                    <MoreInput></MoreInput>
+                    {isMoreInput && <MoreInput onKeyup={(e) => handleValues(e)}></MoreInput>}
                     <div className="flex w-full py-2 text-slate-500">
                         <p className="mx-1 hover:underline cursor-pointer">
                             All (28)
